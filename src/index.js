681a8556ce4df7dd549b41d76c0c970c4db7ebcb
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-10 23:32:45 +0300

    feat: add function min and max

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+    for (i = 0; i < array.length; ++i)
+    if (array[i] < array[i + 1]) min = array[i];
+    else max = array[i];  
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    for (i = 0; i < array.length; ++i)
+    if (array[i] > array[i + 1]) max = array[i];
+    else max = array[i];  
+    return max;
 }
 
 exports.avg = function avg (array) {
