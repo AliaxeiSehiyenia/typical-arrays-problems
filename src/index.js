dc4c7826d453e0e8c77276669728b5c07c97e209
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-11 01:29:01 +0300

    feat: chenge again function min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 exports.min = function min (array) {
-    min_value = array[0];
+    if (array.length == 0) {
+        return 0;
+    }
+    else {
+        min_value = array[0];
     for (i = 1; i < array.length; ++i) {
         if (array[i] < min_value) {
             min_value = array[i];
@@ -8,8 +12,13 @@ exports.min = function min (array) {
     }
     return min_value;
 }
+}
 
 exports.max = function max (array) {
+    if (array.length == 0) {
+        return 0;
+    }
+    else {
     max_value = array[0];
     for (i = 1; i < array.length; ++i) {
         if (array[i] > max_value) {
@@ -18,12 +27,18 @@ exports.max = function max (array) {
     }
     return max_value;
 }
+}
 
 exports.avg = function avg (array) {
+    if (array.length == 0) {
+        return 0;
+    }
+    else {
     var sum = 0;
     var count = array.length;
     for (i = 0; i < count; i++) {
         sum += array[i];
         }
     return sum / count;
+    }
 }
